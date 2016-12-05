49e6c46ab89701848a03598641fd5f84ff20a4ed
wanpeng16 <wanpengwx@163.com>
2016-12-05 17:27:26 +0800

    huffman算法

--- huffman.py
+++ huffman.py
@@ -3,7 +3,7 @@
 from itertools import count
 from heapq import heapify,heappush,heappop
 
-def huffman(seq,freq):
+def huffman(seq,frq):
     num = count()        #生成无限迭代器 1,2,3,....
     trees = list(zip(frq,num,seq))#zip()连接多个可迭代对象[(),(),()]
     heapify(trees)
@@ -14,7 +14,16 @@ def huffman(seq,freq):
         heappush(trees,(fa+fb,n,[a,b]))#通过列表的嵌套表示树结构，生成新的节点
     return trees[0][-1]
 
+def codes(tree,prefix=""):
+    if len(tree)==1:              #递归基
+        yield (tree,prefix)
+        return
+    for bit,child in zip("01",tree):
+        for pair in codes(child,prefix+bit):
+            yield pair
+
 if __name__=="__main__":
     seq="abcdefghi"
     frq=[4,5,6,9,11,12,15,16,20]
-    print huffman(seq,frq)
+    tree=huffman(seq,frq)
+    print list(codes(tree))
