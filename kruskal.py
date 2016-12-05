d48c305123cdcfab69c6e5300ef71a28363e6508
wanpeng16 <wanpengwx@163.com>
2016-12-05 19:14:16 +0800

    注释

--- kruskal.py
+++ kruskal.py
@@ -11,13 +11,15 @@ N = {
     'g':{'a':float('inf'),'b':float('inf'),'c':float('inf'),'d':float('inf'),'e':float('inf'),'f':1,'g':float('inf'),'h':6},
     'h':{'a':float('inf'),'b':float('inf'),'c':float('inf'),'d':float('inf'),'e':float('inf'),'f':9,'g':8,'h':float('inf')}
 }
+#标记解决方案中各点所属的连通域
+#每个节点都指向某个别的节点，并一路延伸下去，直至到达设定的代表节点
 def find(C,u):
     if C[u]!=u:
         C[u]=find(C,C[u])
     return C[u]
 
 def union(C,R,u,v):
-    u=find(C,u);v=find(C,v)
+    u=find(C,u);v=find(C,v)#两个连通域的连接是一个代表节点指向另一个代表节点
     if R[u] > R[v]:
         C[v]=u
     else:
@@ -29,10 +31,13 @@ def Kruskal(G):
     E = [(G[u][v],u,v) for u in G for v in G]   #（权值,u,v）
     T = set()
     C,R = {u:u for u in G},{u:0 for u in G}
-    for _,u,v in sorted(E):
-        if find(C,u) != find(C,v):
+    k =0
+    for _,u,v in sorted(E):#根据边的权值排序
+        if find(C,u) != find(C,v):#判断边的两个点是否属于同一连通域，同一连通域的边将构成环路
             T.add((u,v))
-            union(C,R,u,v)
+            union(C,R,u,v)#新加入的点合并成同一连通域
+            k+=1
+            if k==len(G)-1:break
     return T
 
 if __name__=="__main__":
